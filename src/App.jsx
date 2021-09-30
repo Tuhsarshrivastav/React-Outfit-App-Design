5ddf799889f6bd080f377f64c85ca2260d908daf
Tusharshrivastav <ts3657@gmail.com>
2021-09-30 15:25:19 +0530

    added product screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <ProductList/>
     </>
   );
 }
