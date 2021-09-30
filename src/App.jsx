5902ba518d9bbf83c2a4af87001051c0001161e6
Tusharshrivastav <ts3657@gmail.com>
2021-09-30 15:40:15 +0530

    added register screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 function App() {
   return (
     <>
       {/* <Home /> */}
       {/* <ProductList/> */}
-      <Product />
+      {/* <Product /> */}
+      <Register/>
     </>
   );
 }
