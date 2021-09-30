932ae8598eb65b97bf23848be7d34c3161319bf6
Tusharshrivastav <ts3657@gmail.com>
2021-09-30 12:46:36 +0530

    added navbar & Announcement

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Announcement from "./components/ Announcement";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Announcement />
+      <Home />
+    </>
+  );
+}
+
+export default App;
